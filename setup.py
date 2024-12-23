89f8fdf71945040a92e09a5124301cbb8c1847ad
Marcin Duda <dudamarcin539@gmail.com>
2024-12-23 19:28:02 +0100

    fix: fixing setup.py

--- setup.py
+++ setup.py
@@ -5,25 +5,25 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 version = os.getenv(
-    "VERSION", "0.0.0"
+    "VERSION", "0.0.1"
 )  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
 
 
 
 setup(
-    name="CD",
+    name="CD_zad",
     version=version,
     author="Marcin Duda",
     author_email="dudamarcin539@gmail.com",
     description="Students presence",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Equu1/CD",
+    url="https://github.com/Equu1/CD_zad",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={
-        "console_scripts": ["CD=src:main"],
+        "console_scripts": ["CD_zad=src:main"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
